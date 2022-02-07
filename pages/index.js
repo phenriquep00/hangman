3187e97f5deba7745ad5ecaa8931f728dd3a55c7
Pedro Lima <contact.pedrolima@gmail.com>
2022-02-07 17:32:17 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -4,27 +4,10 @@ import React from 'react';
 import Keyboard from 'react-simple-keyboard';
 import 'react-simple-keyboard/build/css/index.css';
 
-/* aaaaaa */
-function Titulo(props) {
-    const Tag = props.tag;
-    return (
-        <>
-            <Tag>{props.children}</Tag>
-            <style jsx>{`
-        ${Tag} {
-            color: ${appConfig.theme.colors.neutrals[300]};
-            font-size: 24px;
-            font-weight: 600;
-        }
-        `}</style>
-        </>
-
-    )
-}
-
 
 export default function PaginaInicial() {
     const [image, setImage] = React.useState('https://raw.githubusercontent.com/phenriquep00/hangman/master/src/img/0.png');
+    const [lettersPressed, setLettersPressed] = React.useState([]);
 
     return (
         <>
@@ -95,18 +78,60 @@ export default function PaginaInicial() {
                             styleSheet={{
                                 display: 'flex',
                                 alignItems: 'center',
+                                flexDirection: 'column',
                                 minWidth: '300px',
                                 minHeight: '200px',
                                 padding: '10px',
                                 backgroundColor: appConfig.theme.colors.neutrals[100],
                                 border: '1px solid',
-                                borderColor: appConfig.theme.colors.neutrals[999],
+                                borderColor: appConfig.theme.colors.neutrals[100],
                                 borderRadius: '10px',
                                 flex: 1,
                                 margin: '2px 2px 2px 2px'
                             }}
 
                         >
+                            <Box
+                            /* box of letters already pressed */
+                                styleSheet={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    maxWidth: '300px',
+                                    width: "100%",
+                                    maxHeight: '70px',
+                                    padding: '10px',
+                                    backgroundColor: appConfig.theme.colors.neutrals[100],
+                                    border: '1px solid',
+                                    borderColor: appConfig.theme.colors.neutrals[100],
+                                    borderRadius: '10px',
+                                    flex: 1,
+                                    margin: '2px 2px 2px 2px'
+                                }}
+
+                            >
+                                {lettersPressed}
+                            </Box>
+                            {/* box with the hidden word */}
+                            <Box
+                                styleSheet={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    maxWidth: '300px',
+                                    width: "100%",
+                                    maxHeight: '130px',
+                                    padding: '10px',
+                                    backgroundColor: appConfig.theme.colors.neutrals[100],
+                                    border: '1px solid',
+                                    borderColor: appConfig.theme.colors.neutrals[999],
+                                    borderRadius: '10px',
+                                    flex: 1,
+                                    margin: '2px 2px 2px 2px'
+                                }}
+
+                            >
+                                _ _ _ _ _ _ _ _ _
+                            </Box>
+
                         </Box>
                         {/* keyboard box */}
                         <Box
@@ -118,20 +143,36 @@ export default function PaginaInicial() {
                                 padding: '10px',
                                 backgroundColor: appConfig.theme.colors.neutrals[100],
                                 border: '1px solid',
-                                borderColor: appConfig.theme.colors.neutrals[999],
+                                borderColor: appConfig.theme.colors.neutrals[100],
                                 borderRadius: '10px',
                                 flex: 1,
                                 margin: '2px 2px 2px 2px'
                             }}
 
                         >
-                            <Keyboard   
+                            <Keyboard
                                 layout={{
                                     'default': [
                                         'q w e r t y u i o p',
                                         'a s d f g h j k l',
                                         'z x c v b n m t'
-                                      ]
+                                    ]
+                                }}
+                                buttonTheme={[
+                                    {
+                                      class: "hg-red",
+                                      buttons: "a"
+                                    }]}
+                                onKeyPress= {(key) => {
+                                    if (lettersPressed.includes(`${key}`) == false){
+                                        console.log(lettersPressed)
+                                        setLettersPressed([
+                                            ...lettersPressed,
+                                            key
+                                        ]);
+                                    }
+                                    
+                                    
                                 }}
                             />
                         </Box>
